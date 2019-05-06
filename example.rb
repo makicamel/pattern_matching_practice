5fe4e85bbdfef26fcc75cf0d2c510ac4cc218f3c
makicamel <unright@gmail.com>
2019-05-06 23:34:36 +0900

    Add exampleis about splat operator & checking object structure.

--- example.rb
+++ example.rb
@@ -1,4 +1,5 @@
 # Array
+# normal example
 case [0, [1, 2, 3]]
 when [0]
   :unreachable
@@ -8,15 +9,31 @@ when [0, [1, 2, 3]]
   p 'here'
 end
 
+# pattern matching example
 case [0, [1, 2, 3]]
-in [0]
-  :unreachable
 in [0, [1]]
   :unreachable
-in [0, [a, b, c]]
-  p a
-  p b
-  p c
+in [a, b]
+  p a # => 0
+  p b # => [1, 2, 3]
 in [0, [1, 2, 3]]
   :unreachable
+end
+
+# ---
+
+# splat operator
+case [0, [1, 2, 3]]
+in [a, [b, *c]]
+  p a # => 0
+  p b # => 1
+  p c # => [2, 3]
+end
+
+# pattern matching cheks object structure.
+case [0, [1, 2, 3]]
+in [a]
+  :unreachable
+in a
+  p a # => [0, [1, 2, 3]]
 end
